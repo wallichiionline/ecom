887b897b254e7ceaac3bbc1e1bcca109834956a1
Carmen <carmencheney99@gmail.com>
2024-08-15 16:16:37 -0400

    all done

--- HW-a13-Copy/Develop/routes/api/category-routes.js
+++ HW-a13-Copy/Develop/routes/api/category-routes.js
@@ -24,18 +24,59 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"]
+      }
+    ]
+  }).then(data => {
+    res.json(data);
+  }).catch(err => {
+    res.status(418).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({ category_name: req.body.category_name })
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    },
+    }).then(data => {
+      res.json(data);
+    }).catch(err => {
+      res.status(418).json(err);
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    },
+    }).then(data => {
+      res.json(data);
+    }).catch(err => {
+      res.status(418).json(err);
+    })
 });
 
 module.exports = router;
